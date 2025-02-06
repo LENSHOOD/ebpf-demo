fbe7a5530de10007c2d255a6c86bfe4923c10fe6
zhangxuhai <lenshood.zxh@gmail.com>
2025-02-06 13:55:32 +0800

    feat: modify spans to record node and edge separately

--- ebpf-receiver/model.go
+++ ebpf-receiver/model.go
@@ -9,27 +9,63 @@ import (
 	"math/rand"
 )
 
+type Kind int
+
+const (
+	NODE_SRC Kind = iota
+	NODE_DEST
+	EDGE
+)
+const DirectionKey = "direction"
+const SrcSpanName = "tcp_event_src"
+const DestSpanName = "tcp_event_dest"
+const MetadataIp = "k8s.pod.ip"
+const MetadataSrc = "src.ip"
+const MetadataDest = "dest.ip"
+const MetadataPort = "port"
+const MetadataNS = "k8s.namespace.name"
+const MetadataDeployName = "k8s.deployment.name"
+const MetadataNodeName = "k8s.node.name"
+const MetadataPodName = "k8s.pod.name"
+
 func generateEbpfTraces(tcpEvent *TcpEvent) ptrace.Traces {
 	traces := ptrace.NewTraces()
-	resourceSpan := traces.ResourceSpans().AppendEmpty()
-	tcpResource := resourceSpan.Resource()
-	fillResourceWithAttributes(&tcpResource, tcpEvent)
+	traceId := NewTraceID()
 
-	scope := appendScopeSpans(&resourceSpan)
-	appendTraceSpans(&scope)
+	srcRsSpan := traces.ResourceSpans().AppendEmpty()
+	srcRs := srcRsSpan.Resource()
+	fillResourceWithAttributes(&srcRs, tcpEvent, NODE_SRC)
+	srcScope := appendScopeSpans(&srcRsSpan)
+	appendTraceSpans(&srcScope, traceId, SrcSpanName)
+
+	destRsSpan := traces.ResourceSpans().AppendEmpty()
+	destRs := destRsSpan.Resource()
+	fillResourceWithAttributes(&destRs, tcpEvent, NODE_DEST)
+	destScope := appendScopeSpans(&destRsSpan)
+	appendTraceSpans(&destScope, traceId, DestSpanName)
+
+	edgeRsSpan := traces.ResourceSpans().AppendEmpty()
+	edgeRs := edgeRsSpan.Resource()
+	fillResourceWithAttributes(&edgeRs, tcpEvent, EDGE)
+	edgeScope := appendScopeSpans(&edgeRsSpan)
+	appendTraceSpans(&edgeScope, traceId, DestSpanName)
 
 	return traces
 }
 
-func fillResourceWithAttributes(resource *pcommon.Resource, event *TcpEvent) {
+func fillResourceWithAttributes(resource *pcommon.Resource, event *TcpEvent, direction Kind) {
 	attrs := resource.Attributes()
-	// Here we set the "k8s.pod.ip" to be the same as the SrcIP, which helps the subsequent k8s processor in associating
-	// Pod metadata. The SrcIP is sufficient for associating all Pod's metadata, as both sides of Tcp packets are sniffed,
-	// regardless of whether a Pod is at the source or destination, all of them can be accounted at last.
-	attrs.PutStr("k8s.pod.ip", u32ToIPv4(ntoh(event.SrcIP)))
-	attrs.PutStr("src.ip", u32ToIPv4(ntoh(event.SrcIP)))
-	attrs.PutStr("dest.ip", u32ToIPv4(ntoh(event.DstIP)))
-	attrs.PutInt("dest.port", int64(ntohs(event.DstPort)))
+	attrs.PutInt(DirectionKey, int64(direction))
+	switch direction {
+	case NODE_SRC:
+		attrs.PutStr(MetadataIp, u32ToIPv4(ntoh(event.SrcIP)))
+	case NODE_DEST:
+		attrs.PutStr(MetadataIp, u32ToIPv4(ntoh(event.DstIP)))
+		attrs.PutInt(MetadataPort, int64(ntohs(event.DstPort)))
+	case EDGE:
+		attrs.PutStr(MetadataSrc, u32ToIPv4(ntoh(event.SrcIP)))
+		attrs.PutStr(MetadataDest, u32ToIPv4(ntoh(event.DstIP)))
+	}
 }
 
 func appendScopeSpans(resourceSpans *ptrace.ResourceSpans) ptrace.ScopeSpans {
@@ -54,11 +90,11 @@ func NewSpanID() pcommon.SpanID {
 	return spanID
 }
 
-func appendTraceSpans(scopeSpans *ptrace.ScopeSpans) {
+func appendTraceSpans(scopeSpans *ptrace.ScopeSpans, traceId pcommon.TraceID, spanName string) {
 	span := scopeSpans.Spans().AppendEmpty()
-	span.SetTraceID(NewTraceID())
+	span.SetTraceID(traceId)
 	span.SetSpanID(NewSpanID())
-	span.SetName("tcp_event_span")
+	span.SetName(spanName)
 	span.SetKind(ptrace.SpanKindClient)
 	span.Status().SetCode(ptrace.StatusCodeOk)
 }
--- pg-exporter/pg-exporter.go
+++ pg-exporter/pg-exporter.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"github.com/jackc/pgx/v4/pgxpool"
+	ebpfreceiver "github.com/open-telemetry/otelcol-ebpf-demo/epbf-receiver"
 	"go.opentelemetry.io/collector/pdata/pcommon"
 	"go.opentelemetry.io/collector/pdata/ptrace"
 	"go.uber.org/zap"
@@ -24,13 +25,18 @@ func newPgExporter(cfg *PgExporterConfig, logger *zap.Logger) (*postgresExporter
 
 func (e *postgresExporter) pushTraces(ctx context.Context, td ptrace.Traces) error {
 	resourceSpans := td.ResourceSpans()
-	for i := 0; i < resourceSpans.Len(); i++ {
-		resourceSpan := resourceSpans.At(i)
-		if err := e.insertGraph(ctx, resourceSpan.Resource()); err != nil {
-			return err
-		}
+	if err := e.insertGraph(ctx, resourceSpans); err != nil {
+		return err
 	}
 	return nil
+}
+
+func getIntFromRs(rs pcommon.Resource, key string) int64 {
+	v, exist := rs.Attributes().Get(key)
+	if !exist {
+		return 0
+	}
+	return v.Int()
 }
 
 func getStrFromRs(rs pcommon.Resource, key string) string {
@@ -41,39 +47,38 @@ func getStrFromRs(rs pcommon.Resource, key string) string {
 	return v.AsString()
 }
 
-func (e *postgresExporter) insertGraph(ctx context.Context, rs pcommon.Resource) error {
-	srcIp := getStrFromRs(rs, "src.ip")
-	destIp := getStrFromRs(rs, "dest.ip")
-	_, _ = rs.Attributes().Get("dest.port")
-	ns := getStrFromRs(rs, "k8s.namespace.name")
-	deployName := getStrFromRs(rs, "k8s.deployment.name")
-	nodeName := getStrFromRs(rs, "k8s.node.name")
-	podName := getStrFromRs(rs, "k8s.pod.name")
+func (e *postgresExporter) insertGraph(ctx context.Context, rss ptrace.ResourceSpansSlice) error {
+	for i := 0; i < rss.Len(); i++ {
+		rs := rss.At(i).Resource()
+		kind := ebpfreceiver.Kind(getIntFromRs(rs, ebpfreceiver.DirectionKey))
+		switch kind {
+		case ebpfreceiver.NODE_SRC:
+			fallthrough
+		case ebpfreceiver.NODE_DEST:
+			ip := getStrFromRs(rs, ebpfreceiver.MetadataIp)
+			ns := getStrFromRs(rs, ebpfreceiver.MetadataNS)
+			deployName := getStrFromRs(rs, ebpfreceiver.MetadataDeployName)
+			nodeName := getStrFromRs(rs, ebpfreceiver.MetadataNodeName)
+			podName := getStrFromRs(rs, ebpfreceiver.MetadataPodName)
 
-	if err := e.upsertNode(ctx, srcIp, destIp, ns, deployName, nodeName, podName); err != nil {
-		return fmt.Errorf("failed to upsert srcIp into nodes: %w", err)
-	}
-
-	if err := e.upsertEdge(ctx, srcIp, destIp); err != nil {
-		return fmt.Errorf("failed to upsert edge into edges: %w", err)
+			e.logger.Sugar().Debugf("Insert Node: %s", ip)
+			if err := e.upsertNode(ctx, ip, ns, deployName, nodeName, podName); err != nil {
+				return fmt.Errorf("failed to upsert into nodes: %w", err)
+			}
+		case ebpfreceiver.EDGE:
+			srcIp := getStrFromRs(rs, ebpfreceiver.MetadataSrc)
+			destIp := getStrFromRs(rs, ebpfreceiver.MetadataDest)
+			e.logger.Sugar().Debugf("Insert Edge: %s - %s", srcIp, destIp)
+			if err := e.upsertEdge(ctx, srcIp, destIp); err != nil {
+				return fmt.Errorf("failed to upsert edge into edges: %w", err)
+			}
+		}
 	}
 
 	return nil
 }
 
-func (e *postgresExporter) upsertNode(ctx context.Context, srcIp string, destIp string, ns string, deployName string, nodeName string, podName string) error {
-	// upsert destination
-	_, err := e.pool.Exec(ctx, `
-			INSERT INTO nodes (id, mainstat)
-			VALUES ($1, $2)
-			ON CONFLICT (id) 
-			DO UPDATE SET mainstat = $2
-		`, destIp, destIp)
-	if err != nil {
-		return fmt.Errorf("failed to upsert node: %w", err)
-	}
-
-	// upsert source
+func (e *postgresExporter) upsertNode(ctx context.Context, ip string, ns string, deployName string, nodeName string, podName string) error {
 	upsertNodeSql := `
 			INSERT INTO nodes (id, mainstat, secondarystat, title, subtitle)
 			VALUES ($1, $2, $3, $4, $5)
@@ -85,7 +90,7 @@ func (e *postgresExporter) upsertNode(ctx context.Context, srcIp string, destIp 
 			    subtitle = $5
 		`
 
-	_, err = e.pool.Exec(ctx, upsertNodeSql, srcIp, srcIp, podName+"_"+deployName, nodeName, ns)
+	_, err := e.pool.Exec(ctx, upsertNodeSql, ip, ip, deployName+"."+ns, podName, nodeName)
 	if err != nil {
 		return fmt.Errorf("failed to upsert node: %w", err)
 	}
@@ -104,7 +109,7 @@ func (e *postgresExporter) upsertEdge(ctx context.Context, srcIp, destIp string)
 	edgeId := srcIp + "-" + destIp
 	_, err := e.pool.Exec(ctx, upsertEdgeSql, edgeId, srcIp, destIp)
 	if err != nil {
-		return fmt.Errorf("failed to insert edge: %w", err)
+		return fmt.Errorf("failed to upsert edge: %w", err)
 	}
 
 	return nil
